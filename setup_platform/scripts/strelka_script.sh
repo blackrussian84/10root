93a45ba37e56e5afdfc9729e97d476f88aba8b92
Kirill <k@kazakov.xyz>
2024-09-07 21:44:40 +0300

    FIx configs path

--- setup_platform/scripts/strelka_script.sh
+++ setup_platform/scripts/strelka_script.sh
@@ -14,21 +14,22 @@ home_path=$1
 SERVICE_NAME="strelka"
 SRC_DIR="$home_path/resources/$SERVICE_NAME"
 CURR_DIR=$(pwd)
-mkdir -p "${CURR_DIR}/${SERVICE_NAME}"
 
+mkdir -p "${CURR_DIR}/${SERVICE_NAME}"
+cd "${CURR_DIR}/${SERVICE_NAME}"
 
 printf "Copying the Strelka stack configs...\n"
-if [[ -d "$CURR_DIR/configs" ]]; then
+if [[ -d "configs" ]]; then
   read -p "The $SERVICE_NAME configs already exists in the resources directory. Would you like to overwrite them? (y/n): " overwrite
   if [[ "$overwrite" == "y" ]]; then
-    cp -Rf "$SRC_DIR/configs" "${CURR_DIR}/${SERVICE_NAME}"
+    cp -Rf "$SRC_DIR/configs" .
   fi
 else
-  cp -Rf "$SRC_DIR/configs" "${CURR_DIR}/${SERVICE_NAME}"
+  cp -Rf "$SRC_DIR/configs" .
 fi
 
 printf "Copying the docker compose file...\n"
-cp "$SRC_DIR/docker-compose.yml" "${CURR_DIR}/${SERVICE_NAME}/"
+cp "$SRC_DIR/docker-compose.yml" .
 
 # NOTE: --force-recreate is used to ensure that the containers are recreated with the new configs
 docker compose up -d --force-recreate
