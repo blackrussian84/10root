aa7729cb5a8d27306c298424d148983ecf2b1cff
Kirill <k@kazakov.xyz>
2024-09-07 21:36:29 +0300

    UPD strelka script

--- setup_platform/scripts/strelka_script.sh
+++ setup_platform/scripts/strelka_script.sh
@@ -11,57 +11,23 @@ if [ -z "$1" ]; then
 fi
 
 home_path=$1
-STRELKA_GIT_COMMIT=${STRELKA_GIT_COMMIT:-"5a5fc63a0d7505eb9db568e9344ad06d54800054"}
-STRELKA_UI_GIT_COMMIT=${STRELKA_UI_GIT_COMMIT:-"024610a88ddfec9f441cf7f52bc4a666536e60ad"}
+SERVICE_NAME="strelka"
+SRC_DIR="$home_path/resources/$SERVICE_NAME"
+CURR_DIR=$(pwd)
 
-# Step 1: Clone the Strelka UI repository and check out the specific commit
-printf "Cloning the Strelka UI repository and checking out commit %s...\n" "$STRELKA_UI_GIT_COMMIT"
-if [ -d "strelka-ui" -a -d "strelka-ui/.git" ]; then
-  cd strelka-ui
-  git reset --hard
-  git pull origin "$STRELKA_UI_GIT_COMMIT" --rebase
+printf "Copying the Strelka stack configs...\n"
+if [[ -d "$CURR_DIR/configs" ]]; then
+  read -p "The $SERVICE_NAME configs already exists in the resources directory. Would you like to overwrite them? (y/n): " overwrite
+  if [[ "$overwrite" == "y" ]]; then
+    cp -Rf "$SRC_DIR/configs" "$CURR_DIR/"
+  fi
 else
-  git clone https://github.com/target/strelka-ui.git
-  cd strelka-ui
-fi
-git checkout "$STRELKA_UI_GIT_COMMIT"
-
-# Step 2: Update the homepage in package.json and config.js
-#TMP disable this step to use rewrite on the nginx side
-cd ui
-sed -i 's/"homepage": "\/"/"homepage": "\/strelka"/g' package.json
-if grep -q '"homepage": "/strelka"' package.json; then
-   echo "Update homepage successful"
-else
-   echo "Update homepage failed"
+  cp -Rf "$SRC_DIR/configs" "$CURR_DIR/"
 fi
 
-sed -i "s|baseUrl: '/'|baseUrl: '/strelka'|" public/config.js
+printf "Copying the docker compose file...\n"
+cp "$SRC_DIR/docker-compose.yml" "$CURR_DIR/"
 
-# Step 3: Build the Strelka UI Docker image
-cd ..
-docker compose build
-cd ..
-
-# Step 4: Clone the Strelka repository and check out the specific commit
-printf "Cloning the Strelka repository and checking out commit %s...\n" "$STRELKA_GIT_COMMIT"
-if [ -d "strelka" -a -d "strelka/.git" ]; then
-  cd strelka
-  git reset --hard
-  git pull origin "$STRELKA_GIT_COMMIT" --rebase
-else
-  git clone https://github.com/target/strelka.git
-  cd strelka
-fi
-git checkout "$STRELKA_GIT_COMMIT"
-
-# Step 5: Copy the docker-compose.yaml file from the specified home_path
-printf "Copying docker-compose.yaml from %s...\n" "$home_path"
-cp "$home_path/resources/strelka/docker-compose.yaml" build/docker-compose.yaml
-
-# Step 6: Build and bring up the Strelka services
-printf "Building and bringing up the Strelka services...\n"
-docker compose -f build/docker-compose.yaml build
-docker compose -f build/docker-compose.yaml up -d
-
+# NOTE: --force-recreate is used to ensure that the containers are recreated with the new configs
+docker compose up -d --force-recreate
 printf "Strelka deployment completed successfully.\n"
