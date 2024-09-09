2de57ebe12b81fe827a4850e3be73415946bcc82
Kirill <k@kazakov.xyz>
2024-09-09 21:57:04 +0300

    Small improvement

--- setup_platform/scripts/cleanup.sh
+++ setup_platform/scripts/cleanup.sh
@@ -5,11 +5,20 @@ sudo docker rm $(sudo docker ps -a -q)
 sudo docker volume rm iris-web_db_data
 sudo docker network prune
 
-sudo rm -rf /home/Eleven1978/setup_platform/scripts/strelka
-sudo rm -rf /home/Eleven1978/setup_platform/scripts/nginx
-sudo rm -rf /home/Eleven1978/setup_platform/scripts/portainer
-sudo rm -rf /home/Eleven1978/setup_platform/scripts/docker-elk
-sudo rm -rf /home/Eleven1978/setup_platform/scripts/velociraptor-docker
-sudo rm -rf /home/Eleven1978/setup_platform/scripts/timesketch
-sudo rm -rf /home/Eleven1978/setup_platform/scripts/strelka-ui
-sudo rm -rf /home/Eleven1978/setup_platform/scripts/iris-web
+source "../resources/default.env"
+# If the username is not defined, then ask user to enter the username
+if [ -z "$username" ]; then
+    current_user=$(whoami)
+    read -p "Enter username for home directory setup (default: $current_user): " username
+    username=${username:-$current_user}
+fi
+scripts_path="/home/$username/setup_platform/scripts"
+
+sudo rm -rf "${scripts_path}/docker-elk"
+sudo rm -rf "${scripts_path}/iris-web"
+sudo rm -rf "${scripts_path}/nginx"
+sudo rm -rf "${scripts_path}/portainer"
+sudo rm -rf "${scripts_path}/strelka"
+sudo rm -rf "${scripts_path}/strelka-ui"
+sudo rm -rf "${scripts_path}/timesketch"
+sudo rm -rf "${scripts_path}/velociraptor-docker"
